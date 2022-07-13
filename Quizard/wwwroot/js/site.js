e1b2342134a041cf83ed8190a4b34649b79329c1
Harry N <harry@live.fi>
2022-07-13 13:30:40 +0100

    Sorting Added
    - Sorting via vanilla JS added
    - Bugs: Dragging up has issues, sorting section needs revising

--- Quizard/wwwroot/js/site.js
+++ Quizard/wwwroot/js/site.js
@@ -1,31 +1,161 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿"use strict";
 
 
+// ACCORDION QUESTION
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+var accordionQuestionButton = document.querySelector(
+    "accordion-question-button"
+);
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+document.querySelectorAll(".accordion-question-button").forEach((item) => {
+    item.addEventListener("click", (event) => {
+        // console.log("click check");
+        let accordionQuestionCollapse = item.nextElementSibling;
+
+        if (!item.classList.contains("collapsing")) {
+            if (!item.classList.contains("open")) {
+                // if has class open
+                // remove collapse, add collapsing to class .accordion-collapse (sibling)
+                // after x no of time, remove collapsing class and add collapse open class
+                // open accordion item
+                // console.log("toggle acc button");
+
+                // set height
+                accordionQuestionCollapse.style.display = "block";
+                let accHeight = accordionQuestionCollapse.clientHeight;
+
+                setTimeout(() => {
+                    accordionQuestionCollapse.style.height = accHeight + "px";
+                    accordionQuestionCollapse.style.display = "";
+                }, 1);
+
+                accordionQuestionCollapse.classList =
+                    "accordion-question-collapse collapsing";
+
+                setTimeout(() => {
+                    //   console.log("opening");
+                    accordionQuestionCollapse.classList =
+                        "accordion-question-collapse collapse open";
+                    // accCollapse.style.height = "";
+                }, 300);
             }
+            //close accordion item
+            // if doesnt have class open
+            // remove class open from .acc-collapse, add collapsing
+            // after x no of time remove collapsing and add collapse
+            else {
+                let accCollapse = item.nextElementSibling;
+
+                accCollapse.classList = "accordion-question-collapse collapsing";
+
+                setTimeout(() => {
+                    accCollapse.style.height = "0px";
+                }, 1);
+
+                setTimeout(() => {
+                    //   console.log("closing");
+                    accCollapse.classList = "accordion-question-collapse collapse";
+                    accCollapse.style.height = "";
+                }, 300);
+            }
+
+            item.classList.toggle("open");
         }
-    }
+    });
+});
+
+// DRAG AND DROP QUESTIONS
+
+var draggableQuestions = document.querySelectorAll(
+    ".accordion-question-item"
+);
+var containerSections = document.querySelectorAll(".accordion-section-item");
+
+draggableQuestions.forEach((draggable) => {
+    draggable.addEventListener("dragstart", () => {
+        draggable.classList.add("dragging");
+    });
+
+    draggable.addEventListener("dragend", () => {
+        draggable.classList.remove("dragging");
+    });
+});
+
+containerSections.forEach((container) => {
+    container.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        const afterElement = getDragAfterElement(container, e.clientY);
+        const draggable = document.querySelector(".dragging");
+        if (afterElement == null) {
+            container.appendChild(draggable);
+        } else {
+            container.insertBefore(draggable, afterElement);
+        }
+    });
+});
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [
+        ...container.querySelectorAll(".draggable:not(.dragging)"),
+    ];
+
+    return draggableElements.reduce(
+        (closest, child) => {
+            const box = child.getBoundingClientRect();
+            const offset = y - box.top - box.height / 2;
+            if (offset < 0 && offset > closest.offset) {
+                return { offset: offset, element: child };
+            } else {
+                return closest;
+            }
+        },
+        { offset: Number.NEGATIVE_INFINITY }
+    ).element;
 }
 
+// DRAG AND DROP SECTION
 
-    //Dragable List:
+// const draggableSections = document.querySelectorAll(".accordion-section-item");
+// const containerQuiz = document.querySelectorAll(".quiz-wrapper");
 
+// draggableSections.forEach((draggable) => {
+//   draggable.addEventListener("dragstart", () => {
+//     draggable.classList.add("dragging");
+//   });
 
+//   draggable.addEventListener("dragend", () => {
+//     draggable.classList.remove("dragging");
+//   });
+// });
+
+// containerQuiz.forEach((container) => {
+//   container.addEventListener("dragover", (e) => {
+//     e.preventDefault();
+//     const afterElement = getDragAfterElement(container, e.clientY);
+//     const draggable = document.querySelector(".dragging");
+//     if (afterElement == null) {
+//       container.appendChild(draggable);
+//     } else {
+//       container.insertBefore(draggable, afterElement);
+//     }
+//   });
+// });
+
+// function getDragAfterElement(container, y) {
+//   const draggableElements = [
+//     ...container.querySelectorAll(".draggable:not(.dragging)"),
+//   ];
+
+//   return draggableElements.reduce(
+//     (closest, child) => {
+//       const box = child.getBoundingClientRect();
+//       const offset = y - box.top - box.height / 2;
+//       if (offset < 0 && offset > closest.offset) {
+//         return { offset: offset, element: child };
+//       } else {
+//         return closest;
+//       }
+//     },
+//     { offset: Number.NEGATIVE_INFINITY }
+//   ).element;
+// }
